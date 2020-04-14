088c8b08851854e0fa31b4bb046c12ec3690e27f
Vincent Gabb <60416073+gabbvincent@users.noreply.github.com>
2020-04-13 20:27:28 -0700

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ package main
 
 import "fmt"
 //Create function to determine commission based on sales price
-func Commission(a int)(string, int){
+func Commission(a int)(int){
   if a <= 50000{ //if price is 50k or less, then 4% commission
-    return "your commission is $", (a*4)/100
+    return (a*4)/100
   } else if a >50000 && a<250000{ // if sales price is more than 50k and less than or equal to 250k, then 6% commission.
-    return "your commission is $", (a*6)/100
+    return (a*6)/100
   } else { //anyother price higher than 250k gets 6.75% commission
-    return "your commission is $", (a*675/100)/100
+    return (a*675/100)/100
   }
 }
 
@@ -23,5 +23,5 @@ func main() {
   fmt.Println("Enter in the seeling price of the item you sold")
   fmt.Scanln(&a)
   //call commission(a)
-  fmt.Println(Commission(a))
+  fmt.Println("Your earned $",Commission(a),"in Commission on that sale!")
 }
